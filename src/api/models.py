980c4bc4b0ad23345dc1ab6f660a967c3d0f104b
Alexanderwenger <awengern@gmail.com>
2022-09-03 17:02:58 +0000

    actualizacion registro back

--- src/api/models.py
+++ src/api/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -15,5 +16,26 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Registro(db.Model):
+    __tablename__ = 'registro'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    userName = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'<Registro {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "userName": self.userName,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,20 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+    
+
+@api.route('/registro', methods=['POST'])
+def registro():
+    body = request.get_json()
+    #coprobar si existe un usuario con mismo correo
+    one_people = User.query.filter_by(email=body['email']).first()
+    if one_people:
+        return jsonify({"error": "ya existe"}),418
+    else:
+        usuarioNuevo = User(email=body['email'], password=body['password'], is_active=True) 
+        db.session.add(usuarioNuevo)
+        db.session.commit()
+        return jsonify(body),201 # created
+
+
+
